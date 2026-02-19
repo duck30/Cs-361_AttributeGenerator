cb4b6a14db695c4b6d64e8c10dd29612550aaae6
Carlos Duque <duquec@oregonstate.edu>
2026-02-18 20:10:39 -0800

    added going through Pokeattributes and getting attributes for type1, type2, and legendary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-
+import random
 
 
 
@@ -27,10 +27,14 @@ def main():
     # attributes["PokeAttributes"] will give you the pokemon attribute dictionary
     # by itself. then you can treat it like any other python library.
     #################################################################################
-
-
-
-
+    #create shallow copy of "PokeAttributes"
+    att = attributes["PokeAttributes"]
+    # 18 (0-17) atrributes for type1
+    type1_attribute = att["type1"][random.randint(0, 17)]
+    # 19 (0-18) for type2
+    type2_attribute = att["type2"][random.randint(0, 18)]
+    # 2  (0-1)for legendary
+    legendary_attribute = att["legendary"][random.randint(0, 1)]
 
 
 
